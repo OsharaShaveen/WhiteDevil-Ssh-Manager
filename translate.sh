1febbab8c111f6bea067332f36820b6ea01bce89
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2021-12-02 20:57:40 +0530

    Add files via upload

---
+++ translate.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+clear
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;32m-        TRANSLATE BY WhiteDevil🇱🇰            -\033[0m"
+echo -e "\033[0;32m-              Powerd By SVF MAIN Team 🇱🇰     -\033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+sleep 2
+echo "Initializing............."
+sleep 3
+echo "Translating............."
+sleep 2
+echo "Please Wait..............."
+echo ""
+echo -ne '[🇱🇰                       ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/addhost -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰                      ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/alterarlimite -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰                     ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/alterarsenha -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰                    ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/attscript -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                   ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/badvpn -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                  ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/banner -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                 ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/blockt -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/botssh -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰               ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/conexao -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰              ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/criarteste -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰             ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/criarusuario -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰            ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/delhost -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰           ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/delscript -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰          ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/detalhes -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰         ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/expcleaner -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰        ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/infousers -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰       ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/menu -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰      ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/mudardata -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰     ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/otimizar -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰    ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/reiniciarservicos -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰   ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/cmd/reiniciarsistema -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰  ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/remover -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰 ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/senharoot -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/sshmonitor -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/userbackup -P /tmp/SSH-PLUS-ENG/
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰]\r'
+echo ""
+echo ""
+echo ""
+for filename in /tmp/SSH-PLUS-ENG/*; do
+    cp $filename /bin/
+done
+
+rm /tmp/SSH-PLUS-ENG/*
+
+echo "Translation COMPLETE"
+sleep 1
+echo "Thank you for using this Script.Special Rspect My Master 😈 He Is @WhiteDevilOp999"
+sleep 2
+echo "exiting................"
+echo ""
+sleep 3
+
