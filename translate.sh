d2c7f75c75052633b3588fac62c8a7b4708e143d
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2021-12-02 17:34:26 +0530

    Update translate.sh

--- translate.sh
+++ translate.sh
@@ -2,8 +2,8 @@
 clear
 echo ""
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[0;32m-        TRANSLATE BY PHC_MARED0 PHC_Jayvee      -\033[0m"
-echo -e "\033[0;32m-              REQUESTED BY CUPCAKE_13           -\033[0m"
+echo -e "\033[0;32m-        TRANSLATE BY WhiteDevil🇱🇰            -\033[0m"
+echo -e "\033[0;32m-              Powerd By SVF MAIN Team 🇱🇰     -\033[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 sleep 2
@@ -13,77 +13,77 @@ echo "Translating............."
 sleep 2
 echo "Please Wait..............."
 echo ""
-echo -ne '[#                       ]\r'
+echo -ne '[🇱🇰                       ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/addhost -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[##                      ]\r'
+echo -ne '[🇱🇰🇱🇰                      ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/alterarlimite -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[###                     ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰                     ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/alterarsenha -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[####                    ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰                    ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/attscript -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[#####                   ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                   ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/badvpn -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[######                  ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                  ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/banner -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[#######                 ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                 ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/blockt -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[########                ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰                ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/botssh -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[#########               ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰               ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/conexao -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[##########              ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰              ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/criarteste -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[###########             ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰             ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/criarusuario -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[############            ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰            ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/delhost -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[#############           ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰           ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/delscript -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[##############          ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰          ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/detalhes -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[###############         ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰         ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/expcleaner -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[################        ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰        ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/infousers -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[#################       ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/menu -P /tmp/SSH-PLUS-ENG/
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰       ]\r'
+wget -q https://raw.githubusercontent.com/OsharaShaveen/WhiteDevil-Ssh-Manager/main/cmd/menu -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[##################      ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰      ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/mudardata -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[###################     ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰     ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/otimizar -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[####################    ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰    ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/reiniciarservicos -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[#####################   ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰   ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/reiniciarsistema -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[######################  ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰  ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/remover -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
-echo -ne '[####################### ]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰 ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/senharoot -P /tmp/SSH-PLUS-ENG/
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/sshmonitor -P /tmp/SSH-PLUS-ENG/
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/userbackup -P /tmp/SSH-PLUS-ENG/
-echo -ne '[########################]\r'
+echo -ne '[🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰🇱🇰]\r'
 echo ""
 echo ""
 echo ""
@@ -95,7 +95,7 @@ rm /tmp/SSH-PLUS-ENG/*
 
 echo "Translation COMPLETE"
 sleep 1
-echo "Thank you for using this Script ..............."
+echo "Thank you for using this Script.Special Rspect My Master 😈"
 sleep 2
 echo "exiting................"
 echo ""
